3274696e23b3b5b4c148e31437152ef804cedab1
Sergey Kucherenko <skv026@bk.ru>
2025-10-02 13:00:53 +0300

    updated variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-title = 'Уроки';
-screens = 2;
-screenPrice = 220.50;
-rollback = false;
-fullPrice = 1200.00;
-adaptive = true;
+const title = 'Уроки';
+let screens = 2;
+let screenPrice = 220.50;
+let rollback = false;
+let fullPrice = 1200.00;
+let adaptive = true;
 
 alert('Продолжать нужно?')
 console.log('Требую продолжения банкета');
