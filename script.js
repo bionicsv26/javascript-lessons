a53a40c8cad3cd8184928e3f4d59916685d70f52
Sergey Kucherenko <skv026@bk.ru>
2025-10-02 21:03:17 +0300

    completed the task for lesson 2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const title = 'Уроки';
-let screens = 2;
-let screenPrice = 220.50;
-let rollback = false;
-let fullPrice = 1200.00;
+const title = 'Продажа смартфонов и планшетов';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 220.50;
+let rollback = 25;
+let fullPrice = 3000.00;
 let adaptive = true;
 
-alert('Продолжать нужно?')
-console.log('Требую продолжения банкета');
+// alert('Продолжать нужно?');
+// console.log('Требую продолжения банкета');
+console.log('Тип данных переменной title - ', typeof title);
+console.log('Тип данных переменной fullPrice - ', typeof fullPrice);
+console.log('Тип данных переменной adaptive - ', typeof adaptive);
+
+console.log('длина строки переменной screens - ', screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log('Процент отката посреднику за работу', fullPrice * (rollback/100));
