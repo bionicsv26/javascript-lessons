5e6022c2fa3c49f616f5a7a421ec840aaccb1a0e
Sergey Kucherenko <skv026@bk.ru>
2025-10-02 12:52:46 +0300

    completed the task for lesson 1

---
+++ script.js
@@ -0,0 +1,9 @@
+title = 'Уроки';
+screens = 2;
+screenPrice = 220.50;
+rollback = false;
+fullPrice = 1200.00;
+adaptive = true;
+
+alert('Продолжать нужно?')
+console.log('Требую продолжения банкета');
